62593652501a91c8181e7b56749212748d5a9f49
Michal <miso.zima@1337tester.com>
2023-05-18 12:56:55 +0200

    update test description

--- cypress/e2e/cv.cy.js
+++ cypress/e2e/cv.cy.js
@@ -1,26 +1,26 @@
 describe('check email', () => {
-  it('passes', () => {
+  it('email is correct', () => {
     cy.visit('https://cv.1337tester.com')
     cy.get('body > div > section > table > tbody > tr:nth-child(1) > td:nth-child(2):contains(\'miso.zima@1337tester.com\')')
   })
 })
 
-describe('check email', () => {
-  it('passes', () => {
+describe('check website', () => {
+  it('website is correct', () => {
     cy.visit('https://cv.1337tester.com')
     cy.get('body > div > section > table > tbody > tr:nth-child(2) > td:nth-child(2):contains(\'www.1337tester.com\')')
   })
 })
 
 describe('check linkedin', () => {
-  it('passes', () => {
+  it('LinedIn is correct', () => {
     cy.visit('https://cv.1337tester.com')
     cy.get('body > div > section > table > tbody > tr:nth-child(3) > td:nth-child(2):contains(\'www.linkedin.com/in/1337tester/\')')
   })
 })
 
-describe('check phone number', () => {
-  it('passes', () => {
+describe('check phone', () => {
+  it('phone is correct', () => {
     cy.visit('https://cv.1337tester.com')
     cy.get('.wrapper > section:nth-child(2) > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(4) > td:nth-child(2):contains(\'+41766121224\')')
   })
